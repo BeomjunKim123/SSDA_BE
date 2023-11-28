2f86f3289d2d6d47ff2274c3482d119b27b6ffe2
ARProxy <hocci0222@kakao.com>
2023-11-28 20:51:05 +0900

    first

---
+++ src/main/java/com/ssda/SsdaApplication.java
@@ -0,0 +1,13 @@
+package com.ssda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssda/SsdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
